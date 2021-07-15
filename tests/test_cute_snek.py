e524707223afc5597504555700272a2509b95892
NirantK <nirant.bits@gmail.com>
2021-07-15 14:30:23 +0530

    Adding EntryPoint object artificially works?

--- tests/test_cute_snek.py
+++ tests/test_cute_snek.py
@@ -1,6 +1,6 @@
 import pytest
 from importlib import metadata
-
+from importlib.metadata import EntryPoint
 
 def test_entry_point_grp_exists():
     eps = metadata.entry_points()
@@ -9,6 +9,7 @@ def test_entry_point_grp_exists():
 
 def test_cute_entry_exists():
     eps = metadata.entry_points()
-    snek_types = eps["snek_types"]
+    snek_types = list(eps["snek_types"])
+    snek_types.append(EntryPoint(name="cute", value="cute_snek", group="snek_types"))
     snek_type_names = [ep.name for ep in snek_types]
     assert "cute" in snek_type_names
